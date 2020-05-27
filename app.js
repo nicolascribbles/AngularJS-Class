658c27a5d02482dfe1ff408e4d6d77fa44573494
Nicola Reyes <nreyes@intrinio.com>
2020-05-27 14:29:45 -0600

    Exploring Angular modules

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // MODULE
-var angularApp = angular.module('angularApp', []);
+var angularApp = angular.module('angularApp', ['ngMessages', 'ngResource']);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {
+angularApp.controller('mainController', function ($scope, $log, $filter, $resource) {
+    $scope.name = 'Nicola';
+    $scope.formattedName = $filter('uppercase')($scope.name);
     
-}]);
-
-
-
+    $log.info($scope.name);
+    $log.info($scope.formattedName);
+    
+    console.log($resource);
+});
