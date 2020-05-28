53adc4741b3ecb02c1969d14d25d8ccdfda17e49
Nicola Reyes <nreyes@intrinio.com>
2020-05-28 11:08:57 -0600

    Learned that this is the proper controller structure for minified files

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 var angularApp = angular.module('angularApp', ['ngMessages', 'ngResource']);
 
 // CONTROLLERS
-angularApp.controller('mainController', function ($scope, $log, $filter, $resource) {
+// For one of my changes in this update for the class, I learned to replace the function with an array where the function lives so that minification doesn't destroy my dependency injections
+angularApp.controller('mainController', ['$scope', '$log', function ($scope, $log, $filter, $resource) {
     $scope.name = 'Nicola';
     $scope.formattedName = $filter('uppercase')($scope.name);
     
@@ -10,4 +11,4 @@ angularApp.controller('mainController', function ($scope, $log, $filter, $resour
     $log.info($scope.formattedName);
     
     console.log($resource);
-});
+}]);
